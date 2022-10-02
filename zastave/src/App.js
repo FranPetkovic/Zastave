9bfb1ae29838648c0db0c200fe5c08c2a5abbf37
Fran Petković <fran.petkovic1@gmail.com>
2022-10-02 23:08:54 +0200

    Sortiranje 1.0

--- zastave/src/App.js
+++ zastave/src/App.js
@@ -5,32 +5,79 @@ import axios from 'axios';
 function App() {
 
   let rezultati1=[]
+  let rezultati2=[]
   const [rezulti, setRezultati] = useState([])
+  const [currentValue, setCurrentValue] = useState("")
+  const [valueState,setValueState] = useState("")
+
+  function handleChange(evt) {
+    setCurrentValue(evt.currentTarget.value)
+  }
+  
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
         'https://restcountries.com/v3.1/all',
-      )
-      rezultati1=[result.data]
-      setRezultati(rezultati1[0])
-    }
+        )
+        rezultati1=[result.data]
+        rezultati2=rezultati1[0]
+        setRezultati(rezultati1[0])
+      }
     fetchData()
   },[])
+  
+  const [currentFruit, setCurrentFruit] = useState("0")
+      
+  const changeFruit = (newFruit) => {
+    setCurrentFruit(newFruit)
+  }
 
+  if(currentFruit==="0"){
+    rezultati1=rezulti
+    rezultati2=rezulti
+    
+    rezultati2.sort((a,b) => (a.name.common > b.name.common) ? 1 : ((b.name.common > a.name.common) ? -1 : 0));
+  }
+  else if(currentFruit==="1"){
+    rezultati1=rezulti
+    rezultati2=rezulti
+    
+    rezultati2.sort((a,b) => (a.name.common > b.name.common) ? 1 : ((b.name.common > a.name.common) ? -1 : 0));
+    rezultati2.reverse();
+  }
+  else if(currentFruit==="2"){
+    rezultati1=rezulti
+    rezultati2=rezulti
+    
+    rezultati2.sort((a,b) => (a.population > b.population) ? 1 : ((b.population > a.population) ? -1 : 0));
+    rezultati2.reverse();
+  }
+  else if(currentFruit==="3"){
+    rezultati1=rezulti
+    rezultati2=rezulti
+    
+    rezultati2.sort((a,b) => (a.population > b.population) ? 1 : ((b.population > a.population) ? -1 : 0));
+  }
 
   return (
     <div>
-        <div className='header'>
+        {/* <div className='header'>
           <h1>Drzave svijeta</h1>
-        </div>
+        </div> */}
         <div className='trazilica'>
-          <input type="text"></input>
+          <input
+            value={currentValue}
+            onChange={(evt) => handleChange(evt)}
+          />
           <div>
-            <select className='option'>
-              <option> Ime: A - Z </option>
-              <option> Ime: Z - A </option>
-              <option> Populacija: silazno </option>
-              <option> Populacija: uzlazno </option>
+            <select className='option'
+            onChange={(event) => changeFruit(event.target.value)}
+            value={currentFruit}
+            >
+              <option value="0"> Ime: A - Z </option>
+              <option value="1"> Ime: Z - A </option>
+              <option value="2"> Populacija: silazno </option>
+              <option value="3"> Populacija: uzlazno </option>
             </select>
             <select className='option'>
               <option> Europa </option>
@@ -62,17 +109,3 @@ function App() {
 }
 
 export default App;
-// function App() {
-
-//   let rezultati1=[]
-//   const [rezulti, setRezultati] = useState([])
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const result = await axios(
-//         'https://restcountries.com/v3.1/all',
-//       )
-//       rezultati1=[result.data]
-//       setRezultati(rezultati1[0])
-//     }
-//     fetchData()
-//   },[])
